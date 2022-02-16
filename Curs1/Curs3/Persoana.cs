79da40ca200568b03317f99ca1b998583d7bd215
Andrei Iordache <dario.iordache@gmail.com>
2022-02-16 21:40:34 +0200

    Committing Curs3 (Cours 3 activity)

---
+++ Curs1/Curs3/Persoana.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs3
+{
+    internal class Persoana
+    {
+        //private string nume;
+        //private string prenume;
+        private int varsta;
+        private string cnp;
+        private bool esteBarbat;
+        public string adresa;
+        public int getVarsta { get { return varsta; } }
+
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public int Varsta { get; set; }
+
+        public string Cnp
+        {
+            get { return cnp; }
+            set { cnp = value; }
+        }
+        public bool EsteBarbat
+        {
+            get { return esteBarbat; }
+            set { esteBarbat = value; }
+        }
+
+        
+        
+
+        /*public void SetNume(string nume)
+        {
+            this.nume = nume;
+        }
+        public string GetPrenume()
+        {
+            return prenume;
+        }
+        public void SetPrenume(string prenume)
+        {
+            this.prenume = prenume;
+        }
+
+        public int GetVarsta()
+        {
+            return varsta;
+        }
+        public void SetVarsta(int varsta)
+        {
+            this.varsta = varsta;
+        }
+
+        public string GetCno()
+        {
+            return cnp;
+        }
+        public void SetCnp(string cnp)
+        {
+            this.cnp = cnp;
+        }
+
+        public bool GetEsteBarbat()
+        {
+            return esteBarbat;
+        }
+        public void SetEsteBarbat(bool esteBarbat)
+        {
+            this.esteBarbat = esteBarbat;
+        }*/
+
+        public void ShowSoemthing()
+        {
+            Console.WriteLine("prenumele meu este: " + Prenume);
+        }
+
+        public void SetNumeSiPrenume(string nume, string prenume)
+        {
+            Nume = nume;
+            Prenume= prenume;
+
+        }
+        public void PrintNumeComplet()
+        {
+            System.Console.WriteLine("Numele si prenumele: " + Nume + " " + Prenume);
+        }
+
+        private string GetNumeComplet()
+        {
+            return Prenume + " " + Nume;
+        }
+        public void SchimbPrenume(string nouPrenume)
+        {
+            Prenume = nouPrenume;
+        }
+
+        public void SchimbaNumePrenumeCNP(string nume, string prenume = " PRENUME", string cnp = "CNP")
+        {
+            this.Nume = nume;
+            this.Prenume = prenume;
+            this.Cnp = cnp;
+        }
+    }
+}
---
+++ Curs1/Curs3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Curs3
+{
+    class Program
+    {
+        private string ala = "bala";
+        //enum este o colectie de constante de tip int (strict int)        
+        static void Main(string[] args)
+        {
+            Elev Andrei = new Elev();
+            Student Dario = new Student();
+            Student Alin = new Student();
+            Persoana p1 = new Persoana();
+            p1.adresa = "Strada Fericirii nr 54";
+            Console.WriteLine(p1.adresa);
+            p1.SetNumeSiPrenume("Ionica", "Vasile");
+            p1.PrintNumeComplet();
+            p1.SchimbPrenume("Petrica");
+            p1.PrintNumeComplet();
+
+            Persoana p4 = new Persoana();
+            p4.SchimbaNumePrenumeCNP("NUME", "PRENUME", "1000");
+            p4.PrintNumeComplet();
+            var numeComplet =  p4.Prenume;
+            Console.WriteLine(numeComplet);
+
+            Persoana pers1 = new Persoana();
+            pers1.Prenume = "Cursant";
+            pers1.Nume = "DoItAcademy";
+
+
+        }
+    }
+
+
+}
