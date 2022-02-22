823d14f31d2540f9298256901a65d3606ee63e4a
Andrei Iordache <dario.iordache@gmail.com>
2022-02-22 10:00:42 +0200

    Committing Curs3Tema2 (part 2/3. Introduction to OOP)

---
+++ Curs1/Curs3Tema2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Curs3Tema2
+{
+    public class Program
+    {
+        public enum SpecializareFacultate
+        {
+            AUTOMATICA = 1,
+            ELECTRONICA = 2,
+            COMUNICATII = 3,
+            AUTO = 4,
+            FINANTE = 5,
+            CONTABILITATE = 6
+        }
+        public static void Main(string[] args)
+        {
+            Student stud1 = new Student();
+            stud1.SetNume("Yu-Pei");
+            stud1.SetPrennume("Sun");
+            stud1.SetCnp("1922111234764");
+            stud1.SetFacultate("Politehnica");
+            stud1.SetVarsta(28);
+            stud1.SetSpecializare(SpecializareFacultate.FINANTE);
+            stud1.SetMedie(9.87f);
+            stud1.PrinteazaInformatiiComplete();
+
+            Student stud2 = new Student();
+            stud2.SetNume("Popescu");
+            stud2.SetPrennume("Ion");
+            stud2.SetCnp("1800518171704");
+            stud2.SetFacultate("Politehnica");
+            stud2.SetVarsta(25);
+            stud2.SetSpecializare(SpecializareFacultate.AUTOMATICA);
+            stud2.SetMedie(8.45f);
+            stud2.PrinteazaInformatiiComplete();
+        }
+    }
+
+}
---
+++ Curs1/Curs3Tema2/Student .cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs3Tema2
+{
+    public class Student
+    {
+        private string nume, prenume, cnp, facultate;
+        private short varsta;
+        private float medie;
+        private Program.SpecializareFacultate specializare;
+
+
+        public string GetNume() { return nume; }
+        public void SetNume(string nume) { this.nume = nume; }
+        public string GetPrenume() { return prenume; }
+        public void SetPrennume(string prenume) { this.prenume = prenume; }
+        public string GetCnp() { return cnp; }
+        public void SetCnp(string cnp) { this.cnp = cnp; }
+        public string GetFacultate() { return facultate; }
+        public void SetFacultate(string facultate) { this.facultate = facultate; }
+        public short GetVarsta() { return varsta; }
+        public void SetVarsta(short varsta) { this.varsta = varsta; }
+        public float GetMedie() { return medie; }
+        public void SetMedie(float medie) { this.medie = medie; }
+        public Program.SpecializareFacultate GetSpecializare() { return specializare; }
+        public void SetSpecializare(Program.SpecializareFacultate specializare) { this.specializare = specializare; }
+
+        public void PrinteazaInformatiiComplete()
+        {
+            Console.WriteLine($"Studentul {nume} {prenume}, cnp {cnp}, cu varsta de {varsta} ani este student la" +
+                $" {facultate}, specializarea {specializare} avand media {medie}");
+        }
+    }
+}
